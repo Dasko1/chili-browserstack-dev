82e2230a312a1c0aee2e6239025b82e69c7fbbe6
ADaskalopoulos <adaskalopoulos@demandbridge.com>
2022-11-16 14:39:31 -0500

    Update to GetOrderNumber.java

--- src/test/java/com/demandbridge/single/GetOrderNumber.java
+++ src/test/java/com/demandbridge/single/GetOrderNumber.java
@@ -1,9 +1,5 @@
 package com.demandbridge.single;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Screenshots;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,16 +10,12 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.screenshot;
-
 public class GetOrderNumber {
 
     @Test
     public void getOrderNumber_Test() throws IOException {
 
-        // Before Log in: get the Welcome message from the CSS element.
+        // Before Log in: get the title & Welcome message from the CSS element & print.
         String url = "https://generic.development.dbenterprise.com/Login.epm";
 
         Document document = Jsoup.connect(url).get();
@@ -40,7 +32,7 @@ public class GetOrderNumber {
 
         System.out.println("\nAfter login");
 
-        // After login
+        // After login: get the text from CSS for the top order number.
 
         // Login the JSoup way
         String url1 = "https://generic.development.dbenterprise.com/approval/ApprovalCenter.epm?nhi=250";
@@ -59,10 +51,10 @@ public class GetOrderNumber {
                 .followRedirects(true)
                 .get();
 
-        // The next line finds the cssElement for "Open source" in the URL in l.13.  The next 2 lines strip the html tags & prints it!
-        Elements cssElement1 = document1.select("#header-shopping-cart");
+        // The next line finds the cssElement for "Shopping cart" in the URL in l.54.  The next 2 lines strip the html tags & prints it!
+        // Prints "Orders Requiring Approval; the Order Number CSS will be #approval_content > table > tbody > tr:nth-child(3) > td:nth-child(1) > a
+        Elements cssElement1 = document1.select("#main_content > span > span");
         Document orderNumber = Jsoup.parse(String.valueOf(cssElement1));
         System.out.println("The order number is: " + orderNumber.text());
-
     }
 }
