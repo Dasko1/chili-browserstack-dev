96bf714bdb18f7b47b269a0a2c896ad23b5fc5bd
ADaskalopoulos <adaskalopoulos@demandbridge.com>
2022-12-22 12:15:25 -0500

    Commit Watermark.java

---
+++ src/test/java/com/demandbridge/single/Watermark.java
@@ -0,0 +1,26 @@
+package com.demandbridge.single;
+
+import com.testautomationguru.utility.PDFUtil;
+
+import java.io.IOException;
+
+public class Watermark {
+
+    public static void main(String[] args) throws IOException {
+
+        PDFUtil pdfUtil = new PDFUtil();
+        String getText = pdfUtil.getText("C:\\Users\\ADaskalopoulos\\Downloads\\COR001.pdf", 1, 1);
+        int pageCount = pdfUtil.getPageCount("C:\\Users\\ADaskalopoulos\\Downloads\\COR004.pdf");
+        if (pageCount == 6){
+            System.out.println("The flipbook has " + pageCount + " pages!");
+        }
+        if (getText.contains("Lo wR es P ro of")){
+            System.out.println("The watermark is present!");
+        }
+        else { System.out.println("Test fails: the watermark not present."); }
+        if (getText.contains("generic.com")){
+            System.out.println("The suffix 'generic.com' is present!");
+        }
+        else { System.out.println("Test fails: the suffix is not present."); }
+    }
+}
